2d4f269ea91b67b2a53c2f4ca05187b60ecd803c
Joseph Isbell <39445858+jisbell347@users.noreply.github.com>
2018-09-18 16:47:26 -0600

    Added debug.log statements to instruct the player. Added min/max variables.

---
+++ NumberWizard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    int max = 100;
+	    int min = 1;
+
+	    Debug.Log("Welcome to number wizard");
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Enter = Correct");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
