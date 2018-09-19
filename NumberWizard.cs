2b651b775061773e32435b7f3d6492c303ba5832
Joseph Isbell <39445858+jisbell347@users.noreply.github.com>
2018-09-19 13:09:13 -0600

    Moved state variables out of start method

--- NumberWizard.cs
+++ NumberWizard.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
+    // variables for max range, min range and guess.
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
 
-	// Use this for initialization
-	void Start () {
-	    int max = 100;
-	    int min = 1;
-
+    // Use this for initialization
+    void Start () {
+	    
 	    Debug.Log("Welcome to number wizard");
         Debug.Log("Pick a number, don't tell me what it is...");
         Debug.Log("The highest number you can pick is: " + max);
@@ -21,8 +23,12 @@ public class NumberWizard : MonoBehaviour {
 	void Update () {
 	    if (Input.GetKeyDown(KeyCode.UpArrow)) {
             Debug.Log("Up Arrow was pressed.");
-   	    } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+	        min = guess;
+            Debug.Log(guess);
+	    } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
             Debug.Log("Down Arrow was pressed");
+	        max = guess;
+            Debug.Log(guess);
 	    } else if (Input.GetKeyDown(KeyCode.Return)) {
             Debug.Log("Enter key was pressed.");
 	    }
